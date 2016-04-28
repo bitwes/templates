9910c4c452ea4fb4ba43d5b6a1800f0015998c03
212551038 <tomharvey.wesley@ge.com>
2016-04-28 15:31:39 -0400

    better subcommand template

--- ruby/subcommand_template.rb
+++ ruby/subcommand_template.rb
@@ -2,19 +2,23 @@
 
 require 'optparse'
 
-$subtext  ="\
-This thing has multiple subcommands.  They are:
+$banners  = {
+  :main => "\
+This thing has multiple subcommands.  You have to use one of them.  They are
+listed below.
 
-asdf           The standard in non-functional options.  The asdf option features
-               easy to type characters and a complete lack of any meaning.
-wasd           The keys to use for movement.
-
-Use the -h option with each of the subcommands for more information.  The following
-options are valid for any subcommand:
+Use the -h option with each of the subcommands for more information.
 
 Usage: sub_command_optparse.rb [options] [subcommand [options]]
 --------
+",
+  :asdf => "\
+This is the asdf help
+",
+  :wasd => "\
+This is the wasd help
 "
+}
 
 def parse_options()
   options = {
@@ -24,50 +28,67 @@ def parse_options()
     :move_keys => 'wasd'
   }
 
-  global = OptionParser.new do |opts|
-    opts.banner = $subtext
-    opts.on('-v', '--verbose', 'Obiligatory verbose option.') do |g|
-      options[:verbose] = g
-    end
-  end
-
-
   subcommands = {
     'asdf' => OptionParser.new do |opts|
-      opts.banner = "Look!! another 'asdf' thing."
+      opts.banner = $banners[:asdf]
       opts.on('-a', '--all', 'All the asdf you want.') do |x|
         options[:all] = x
       end
     end,
 
     'wasd' => OptionParser.new do |opts|
-      opts.banner = 'Your movement keys'
+      opts.banner = $banners[:wasd]
       opts.on('-u', '--use_other OPT', 'Supply another 4 keys for movement') do |x|
         options[:move_keys] = x
       end
     end
   }
 
-  global.order!
-  command = ARGV.shift
-  subcommands[command].order!
+  global_banner = $banners[:main] +
+                   " * " + subcommands.keys.join("\n * ") +
+                   "\n\n## Options"
 
-  options[:command] = command
+  global = OptionParser.new do |opts|
+    opts.banner = global_banner
+    opts.on('-v', '--verbose', 'Obiligatory verbose option.') do |g|
+      options[:verbose] = g
+    end
+  end
+
+  global.order!
+  # print message if a subcommand was not supplied
+  if(ARGV.size == 0)
+    print "This command requires one of the following subcommands.  " +
+         "Use -h option for more information.\n  "
+    puts subcommands.keys.join("\n  ")
+    exit
+  else
+    command = ARGV.shift
+    subcommands[command].order!
+    options[:command] = command
+  end
+
   return options
 end
 
 
 def main()
-  options = parse_options
-  if(options[:verbose])
-    puts "The options:  \n#{options}"
-  end
+  begin
+    options = parse_options
+    if(options[:verbose])
+      puts "The options:  \n#{options}"
+    end
 
-  case options[:command]
-  when 'asdf'
-    puts 'Absolutely nothing!!'
-  when 'wasd'
-    puts 'Movement keys!!'
+    case options[:command]
+    when 'asdf'
+      puts 'This does Absolutely nothing!!'
+    when 'wasd'
+      puts 'Movement keys!!'
+    end
+  rescue OptionParser::MissingArgument => e
+    puts "SUPPLY ALL ARGUMENTS!"
+  rescue OptionParser::InvalidOption => e
+    puts "You've supplied an #{e.to_s}"
   end
 end
 
